bbde491a880b0fbf0c01b6834337030c54627892
nicovaca <nicolavacalebre@gmail.com>
2023-09-14 16:41:18 +0200

    Aggiunte props in ExepenseItem

--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -1,12 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-      <div>
-        <h2>Let's get started!</h2>
-          <ExpenseItem></ExpenseItem>
-      </div>
-  );
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Assicurazione Auto',
+            amount: 564.56,
+            date: new Date(2021, 3, 22)
+        },
+        {
+            id: 'e2',
+            title: 'Bolletta Luce',
+            amount: 215.23,
+            date: new Date(2022, 6, 12)
+        },
+        {
+            id: 'e3',
+            title: 'Bolletta Gas',
+            amount: 64.56,
+            date: new Date(2023, 10, 2)
+        },
+        {
+            id: 'e4',
+            title: 'Spese Condominiali',
+            amount: 100,
+            date: new Date(2022, 5, 18)
+        },
+    ];
+
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            {expenses.map(p => <ExpenseItem title={p.title} amount={p.amount} date={p.date}></ExpenseItem>)}
+        </div>
+    );
 }
 
 export default App;
