0f1dc3e28a7b0bcb4b4fc1f9076dca7852705a1d
nicovaca <nicolavacalebre@gmail.com>
2023-09-18 11:19:17 +0200

    Aggiunto componente ExpensesFilter

--- demo-react/src/components/Expenses/Expenses.js
+++ demo-react/src/components/Expenses/Expenses.js
@@ -1,13 +1,23 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 //sintassi alternativa per scrivere le funzioni (componenti) usando le arrow function
 const Expenses = (props) => {
 
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
-        <Card className="expenses">
-            {props.items.map(p => <ExpenseItem title={p.title} amount={p.amount} date={p.date}></ExpenseItem>)}
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {props.items.map(p => <ExpenseItem title={p.title} amount={p.amount} date={p.date}></ExpenseItem>)}
+            </Card>
+        </div>
     );
 }
 
